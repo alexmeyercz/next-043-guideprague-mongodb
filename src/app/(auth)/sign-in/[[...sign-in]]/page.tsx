ed0ed18c3253a41437f3702936c23b8132f89b09
alexmeyercz <alex.meyer.cz@gmail.com>
2024-03-17 11:58:07 +0100

    Set custom signin, signup pages

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from '@clerk/nextjs'
+import React, { type FC } from 'react'
+
+const f = '⇒ page.tsx (SignInPage):'
+
+const SignInPage: FC = () => {
+  return <SignIn />
+}
+export default SignInPage
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import React, { type FC } from 'react'
+import { SignUp } from '@clerk/nextjs'
+
+const f = '⇒ page.tsx (SignUpPage):'
+
+const SignUpPage: FC = () => {
+  return <SignUp />
+}
+export default SignUpPage
